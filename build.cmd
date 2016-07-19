8c486951f3b96ace37bb1dbb5f87379988e7f5f6
杜晓宇 <noliar@qq.com>
2016-07-19 11:21:09 +0800

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,6 +1,5 @@
 @ECHO OFF
 
-cd submodules/libui
-for /f "delims=" %%i in ('git rev-list --count HEAD') do set DOTNET_BUILD_VERSION=%%i
+for /f "delims=" %%i in ('git -C submodules/libui rev-list --count HEAD') do set DOTNET_BUILD_VERSION=%%i
 
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' %*; exit $LASTEXITCODE"
